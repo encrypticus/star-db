56271ff7235cdc253b5178e01a49c845cbcf0f85
encrypticus <encrypticus@gmail.com>
2020-01-30 20:55:24 +0600

    Updates

---
+++ precache-manifest.b6e40cd271b3d5ae084b2d1e37a48511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48b2e49816c0b45365aadc2313172c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da837e3d38b98fbc02b",
+    "url": "/static/css/main.7633618c.chunk.css"
+  },
+  {
+    "revision": "a7ce6e2934d110945b57",
+    "url": "/static/js/2.e41721d7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e41721d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5da837e3d38b98fbc02b",
+    "url": "/static/js/main.e61cac63.chunk.js"
+  },
+  {
+    "revision": "5b0534ba9a74200d754d",
+    "url": "/static/js/runtime-main.29439069.js"
+  }
+]);
